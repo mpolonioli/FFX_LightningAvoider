40991f9bf4c5167343e7e0f5617312023df37dd8
Pumahawk <Lorenzoga94@gmail.com>
2017-01-28 01:18:01 +0100

    Divide main class in another class called LightningSensor
    I decide to extract code from main class so is more easy add new feature
    to the program.
    I insert WAITININGPIXEL and MAXLIGHTNING so is more easy read
    sourcecode.
    Finally i removed some simple comment.

---
+++ src/LightningSensor.java
@@ -0,0 +1,44 @@
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+public class LightningSensor {
+	
+	final public int WAITINGPIXEL;
+	final public int MAXLIGHTNINGAVOIED;
+	
+	public LightningSensor(int waitinPixel, int maxLightningAvoied){
+		this.WAITINGPIXEL = waitinPixel;
+		this.MAXLIGHTNINGAVOIED = maxLightningAvoied;
+	}
+	
+	public LightningSensor(){
+		this(5000, 210);
+	}
+	
+	public void run() throws AWTException{
+		Point coord;
+		Robot robot = new Robot();
+		int lightningCounter = 0;
+		
+		// wait for this.WAITINGPIXEL seconds to permit the user to point the mouse on the desire pixel
+		robot.delay(this.WAITINGPIXEL);
+		coord = MouseInfo.getPointerInfo().getLocation();
+
+		while (lightningCounter <= this.MAXLIGHTNINGAVOIED) {
+			// green >= 70 means screen flash
+			if (robot.getPixelColor((int) coord.getX(), (int) coord.getY()).getGreen() >= 70) {
+				// press ENTER = X 
+				robot.keyPress(KeyEvent.VK_ENTER);
+				robot.delay(100);
+				robot.keyRelease(KeyEvent.VK_ENTER);
+				lightningCounter++;
+				System.out.println(lightningCounter);
+				// wait to prevent X spamming
+				robot.delay(500);
+			}
+		}	
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,9 @@
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.awt.Point;
-import java.awt.MouseInfo;
 
 public class Main {
 
 	public static void main(String[] args) throws AWTException {
-		
-		Point coord;
-		Robot robot = new Robot();
-		int count = 0;
-		boolean whileCondition = true;
-		
-		// wait for 5 seconds to permit the user to point the mouse on the desire pixel
-		robot.delay(5000);
-		// take coordinate of the pointed pixel
-		coord = MouseInfo.getPointerInfo().getLocation();
-		
-		while (whileCondition) {
-			// green >= 70 means screen flash
-			if (robot.getPixelColor((int) coord.getX(), (int) coord.getY()).getGreen() >= 70) {
-				// press ENTER = X 
-				robot.keyPress(KeyEvent.VK_ENTER);
-				robot.delay(100);
-				robot.keyRelease(KeyEvent.VK_ENTER);
-				// how many lightning avoided since now
-				count++;
-				System.out.println(count);
-				// wait to prevent X spamming
-				robot.delay(500);
-				//  exit the while after 210 lightning avoided
-				if (count == 210) {
-					whileCondition = false;
-				}
-			}
-		}	
+		new LightningSensor().run();
 	}
+	
 }
