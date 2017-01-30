69692a00c8a32e0ecb12062010130bcee5153d40
Pumahawk <Lorenzoga94@gmail.com>
2017-01-30 02:09:26 +0100

    Format with JavaConventions

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,28 @@ import java.awt.AWTException;
 
 public class Main {
 
-	final static public int DEFAULT_WAITING_PIXEL = 5000;
-	final static public int DEFAULT_MAXLIGHTNING_AVOIED = 210;
-	public static int counter;
+    final static public int DEFAULT_WAITING_PIXEL = 5000;
+    final static public int DEFAULT_MAXLIGHTNING_AVOIED = 210;
+    public static int counter;
 
-	public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) throws AWTException {
 
-		int waitingPizel;
-		int maxLightningAvoied;
+	int waitingPizel;
+	int maxLightningAvoied;
 
-		try {
-			String commandValue;
-			waitingPizel = ((commandValue = ParserInput.getCommand("-wp", args)) != null)
-					? ParserInput.parseIntForCommand("-wp", commandValue) : DEFAULT_WAITING_PIXEL;
-			maxLightningAvoied = ((commandValue = ParserInput.getCommand("-ml", args)) != null)
-					? ParserInput.parseIntForCommand("-ml", commandValue) : DEFAULT_WAITING_PIXEL;
-		} catch (ParserInputException e) {
-			System.out.println(e.getMessage());
-			return;
-		}
-		// int counter = 0;
-		new LightningSensor(waitingPizel, maxLightningAvoied)
-				.run(() -> System.out.println("Lightning: " + (++counter)));
+	try {
+	    String commandValue;
+	    waitingPizel = ((commandValue = ParserInput.getCommand("-wp", args)) != null)
+		    ? ParserInput.parseIntForCommand("-wp", commandValue) : DEFAULT_WAITING_PIXEL;
+	    maxLightningAvoied = ((commandValue = ParserInput.getCommand("-ml", args)) != null)
+		    ? ParserInput.parseIntForCommand("-ml", commandValue) : DEFAULT_WAITING_PIXEL;
+	} catch (ParserInputException e) {
+	    System.out.println(e.getMessage());
+	    return;
 	}
+	// int counter = 0;
+	new LightningSensor(waitingPizel, maxLightningAvoied)
+		.run(() -> System.out.println("Lightning: " + (++counter)));
+    }
 
 }
--- src/ParserInput.java
+++ src/ParserInput.java
@@ -1,17 +1,17 @@
 
 public class ParserInput {
-	public static String getCommand(String command, String[] arguments){
-		for(int i = 0; i<arguments.length;i++)
-			if(arguments[i].compareTo(command) == 0)
-				return (i<arguments.length) ? arguments[i+1] : null;
-		return null;
+    public static String getCommand(String command, String[] arguments) {
+	for (int i = 0; i < arguments.length; i++)
+	    if (arguments[i].compareTo(command) == 0)
+		return (i < arguments.length) ? arguments[i + 1] : null;
+	return null;
+    }
+
+    public static int parseIntForCommand(String command, String num) {
+	try {
+	    return Integer.parseInt(num);
+	} catch (NumberFormatException e) {
+	    throw new ParserInputException(command, num);
 	}
-	public static int parseIntForCommand(String command, String num){
-		try{
-			return Integer.parseInt(num);
-		}
-		catch(NumberFormatException e){
-			throw new ParserInputException(command, num);
-		}
-	}
+    }
 }
