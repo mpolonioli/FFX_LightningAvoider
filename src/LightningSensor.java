c134ed6c87030661e53daee8a801cd27eb46a966
Pumahawk <Lorenzoga94@gmail.com>
2017-01-30 02:08:37 +0100

    Add support for java lambda expression

--- src/LightningSensor.java
+++ src/LightningSensor.java
@@ -18,7 +18,7 @@ public class LightningSensor {
 		this(5000, 210);
 	}
 	
-	public void run() throws AWTException{
+	public void run(Runnable function) throws AWTException{
 		Point coord;
 		Robot robot = new Robot();
 		int lightningCounter = 0;
@@ -35,7 +35,7 @@ public class LightningSensor {
 				robot.delay(100);
 				robot.keyRelease(KeyEvent.VK_ENTER);
 				lightningCounter++;
-				System.out.println(lightningCounter);
+				function.run();
 				// wait to prevent X spamming
 				robot.delay(500);
 			}
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,26 @@ public class Main {
 
 	final static public int DEFAULT_WAITING_PIXEL = 5000;
 	final static public int DEFAULT_MAXLIGHTNING_AVOIED = 210;
+	public static int counter;
 
 	public static void main(String[] args) throws AWTException {
-		try{
+
+		int waitingPizel;
+		int maxLightningAvoied;
+
+		try {
 			String commandValue;
-			int waitingPizel =  ((commandValue = ParserInput.getCommand("-wp", args)) != null) ? ParserInput.parseIntForCommand("-wp", commandValue) : DEFAULT_WAITING_PIXEL;
-			int maxLightningAvoied = ((commandValue = ParserInput.getCommand("-ml", args)) != null) ? ParserInput.parseIntForCommand("-ml", commandValue) : DEFAULT_WAITING_PIXEL;
-			
-			new LightningSensor(waitingPizel, maxLightningAvoied).run();
-		}
-		catch(ParserInputException e){
+			waitingPizel = ((commandValue = ParserInput.getCommand("-wp", args)) != null)
+					? ParserInput.parseIntForCommand("-wp", commandValue) : DEFAULT_WAITING_PIXEL;
+			maxLightningAvoied = ((commandValue = ParserInput.getCommand("-ml", args)) != null)
+					? ParserInput.parseIntForCommand("-ml", commandValue) : DEFAULT_WAITING_PIXEL;
+		} catch (ParserInputException e) {
 			System.out.println(e.getMessage());
+			return;
 		}
+		// int counter = 0;
+		new LightningSensor(waitingPizel, maxLightningAvoied)
+				.run(() -> System.out.println("Lightning: " + (++counter)));
 	}
-	
+
 }
